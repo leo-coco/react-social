aa0e1780e22ba19b68d218bebc08b1e6f8d69670
Leo Clipet <clipetl@gmail.com>
2024-08-16 13:38:47 -0400

    FIX - Warning children deprecated

--- client/src/features/header/Header.tsx
+++ client/src/features/header/Header.tsx
@@ -1,26 +1,36 @@
-import type React from 'react';
+import { Layout, Menu } from 'antd';
 import { Link } from 'react-router-dom';
 import { UsersContainer } from '../users/Users';
-import { Layout, Menu } from 'antd';
 
 const { Header: AntHeader } = Layout;
 
-const Header: React.FC = () => {
-  return (
-    <AntHeader>
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-        <Menu.Item key="1">
-          <Link to="/">Home</Link>
-        </Menu.Item>
-        <Menu.Item key="2">
-          <Link to="/chat">Chat</Link>
-        </Menu.Item>
-        <div style={{ marginLeft: 'auto' }}>
-        Logged in as: <UsersContainer />
-      </div>
-      </Menu>
-    </AntHeader>
-  );
-}
+const menuItems = [
+  {
+    key: '1',
+    label: <Link to="/">Home</Link>,
+  },
+  {
+    key: '2',
+    label: <Link to="/chat">Chat</Link>,
+  },
+  {
+    key: '3',
+    label:   <div style={{ marginLeft: 'auto', color: 'white' }}>
+    Logged in as: <UsersContainer />
+  </div>,
+  },
+];
 
-export default Header;
+const MyHeader = () => (
+  <AntHeader>
+    <Menu
+      theme="dark"
+      mode="horizontal"
+      defaultSelectedKeys={['1']}
+      items={menuItems}
+    />
+  
+  </AntHeader>
+);
+
+export default MyHeader;
