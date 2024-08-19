eb1fa3af9821087853c46fd922affddb4667a9ae
Leo Clipet <clipetl@gmail.com>
2024-08-19 18:57:58 -0400

    Set vitest for both client and server

---
+++ client/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import { join } from 'path';
+
+// Client-specific configuration
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: join(__dirname, 'src/setupTests.ts'),
+    mockReset: true,
+  },
+});
